a635591dc9d5634314daf2a2f2f032b0d3cf0d25
Fuyang Liu <liufuyang@users.noreply.github.com>
2018-09-09 17:52:31 +0200

    Fix up

--- eks-demo/providers.tf
+++ eks-demo/providers.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-west-2"
+  region     = "us-west-2"
+  access_key = "${var.AWS_ACCESS_KEY}"
+  secret_key = "${var.AWS_SECRET_KEY}"
 }
 
 data "aws_region" "current" {}
